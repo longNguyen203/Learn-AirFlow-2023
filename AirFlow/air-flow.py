4ced0a35eac00169725a74ce2b70802c4b74cf39
LongDE2003 <longlovers2003@gmail.com>
2023-10-12 00:27:46 +0700

    Update

--- AirFlow/air-flow.py
+++ AirFlow/air-flow.py
@@ -1,3 +1,27 @@
 lst = [1,4,7,3]
 result = list(map(lambda x: x**2, lst))
+
 print(result)
+
+result2 = [i for i in lst if i % 2 == 0]
+print(result2)
+
+lst2 = [(1, "c"), (2, "b"), (3, "a")]
+lst2.sort(key=lambda i: i[1])
+print(lst2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
